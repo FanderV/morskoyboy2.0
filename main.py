01cc0e4e521d66a58eb6e379c96c823c48ac6d1b
FanderV <113282846+FanderV@users.noreply.github.com>
2022-11-29 00:33:52 +0300

    буквы и цифры на сетках

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pygame
 
-Window_size = 30
+Window_size = 30 #block_size
 Left_field = 40
 Up_field = 30
 
 Black = (0, 0, 0)
 White = (255, 255, 255)
-
+Blue = (51, 153, 255)
 Size = (Left_field + 26.5 * Window_size, Up_field + 12 * Window_size)  # размер окна
 
 pygame.init()
@@ -19,6 +19,8 @@ Font = pygame.font.SysFont('Times New Roman', Font_size)
 
 
 def draw_cells():
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
     for y in range(11):
         for x in range(11):
             # горизонтальные линии
@@ -34,7 +36,21 @@ def draw_cells():
             pygame.draw.line(Display, Black, (Left_field + y * Window_size + 15 * Window_size, Up_field),
                              (Left_field + y * Window_size + 15 * Window_size, Up_field + 10 * Window_size),
                              1)
+        if y < 10:
+            num_ver = Font.render(str(y+1), True, Blue)
+            letters_hor = Font.render(letters[y], True, Blue)
+            num_ver_width = num_ver.get_width()
+            num_ver_height = num_ver.get_height()
+            letters_hor_width = letters_hor.get_width()
+            #вертикальные сетки 1
+            Display.blit(num_ver, (Left_field - (Window_size//2+num_ver_width//2), Up_field + y*Window_size + (Window_size//2 - num_ver_height//2)))
+            #вертикальные сетки 1
+            Display.blit(letters_hor, (Left_field + y*Window_size+(Window_size//2 - letters_hor_width//2), Up_field + 10 * Window_size))
 
+            # вертикальные сетки 2
+            Display.blit(num_ver, (Left_field - (Window_size // 2 + num_ver_width // 2) +15 * Window_size, Up_field + y * Window_size + (Window_size // 2 - num_ver_height // 2)))
+            # вертикальные сетки 2
+            Display.blit(letters_hor, (Left_field + y * Window_size + (Window_size // 2 - letters_hor_width // 2) + 15 * Window_size, Up_field + 10 * Window_size))
 
 def main():
     game_over = False
